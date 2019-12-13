fa5803559e390dd036082c63dd7ac23a70a64ea5
Maarja Roots <codenation@Codes-MacBook-Pro.local>
2019-12-13 17:06:53 +0000

    Five day forecast

--- routes/routes.js
+++ routes/routes.js
@@ -60,25 +60,32 @@ router.get('/', async (req, res) => {       // took iot from index.js, commeted 
 
   router.post('/fiveDayWeather', async (req, res) => {    //if i click submit button, it will be coming to here. Async needs to wait for the weather function
     let location = req.body.location;  //Body parser takes data and turns it into an object called body. this is the deafult
-    console.log(location);
+    // console.log(location);
     let countryCode = req.body.countryCode;
   
     let data = await getFiveDayWeather(location, countryCode);
     // we know what we are getting
-    console.log(data);
+    // console.log(data);
   
     if (data.list[0]) {
-  
-      let City = data.city.name;
-      let Country = data.city.country
-      let Temperature = data.list[0].main.temp;
-      let Wind = data.list[0].wind.speed;
-      let Cloud = data.list[0].clouds.all;
-      let iconData = data.list[0].weather[0].icon
-      let iconUrl = `http://openweathermap.org/img/wn/${iconData}@2x.png`
-      let Description = data.list[0].weather[0].description
-                          // we are acessing an object inside an object
-      res.render('fiveDayWeather', { data: {City, Country, Temperature, Wind, Cloud, Description }, iconUrl });
+ 
+      let fiveDay = {}
+
+      for(let i = 4; i < data.list.length; i+=8) {
+
+        let iconData = data.list[i].weather[0].icon;
+
+        fiveDay[new Date(data.list[i].dt*1000)] = {
+          Temperature : data.list[i].main.temp,
+          Wind : data.list[i].wind.speed,
+          Cloud : data.list[i].clouds.all,
+          iconUrl :`http://openweathermap.org/img/wn/${iconData}@2x.png`
+        }
+      }
+      let city = data.city.name;
+      let country = data.city.country
+
+      res.render('fiveDayWeather', { data: fiveDay, city, country }); //{City, Country, Temperature, Wind, Cloud, Description }
   
     } else {
   
